68b4c22940ccce641d3aa36e3bc67f544243ce92
David Rushton <david-rushton@outlook.com>
2018-11-29 21:40:21 +0000

    Configured module psd1 and psm1 files.

--- Source/PowMark/PowMark.psm1
+++ Source/PowMark/PowMark.psm1
@@ -1,4 +1,25 @@
-# Implement your module commands in this script.
+#
+# PowMark Module
+#
+
+
+# Get public, private and class definition files.
+$classes = @( Get-ChildItem -Path $PSScriptRoot\Classes\*.ps1 -ErrorAction SilentlyContinue )
+$public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1  -ErrorAction SilentlyContinue )
+$private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
+
+# Dot source the files
+Foreach($import in @($classes + $private + $public))
+{
+    Try
+    {
+        . $import.FullName
+    }
+    Catch
+    {
+        Throw "Failed to import $($import.fullname): $_"
+    }
+}
 
 
 # Export only the functions using PowerShell standard verb-noun naming.
