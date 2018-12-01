dd19a98329c79ad8564f24b17d6ade063a966cb2
David Rushton <david-rushton@outlook.com>
2018-12-01 23:38:21 +0000

    Added build script.

--- Scripts/Build.ps1
+++ Scripts/Build.ps1
@@ -1,1 +1,63 @@
-#  publish-module -Path "$PSScriptRoot\Source\PowMark" -Repository $Repo
+#
+# Build Scripts
+# Run test, build and deploy
+#
+
+[CmdletBinding()]
+param(
+
+    [parameter(
+        ValueFromPipeline,
+        ValueFromPipelineByPropertyName,
+        HelpMessage='Name of PowerShell repository to publish module to.'
+    )]
+    [string]$Repository,
+
+    [parameter(
+        ValueFromPipeline,
+        ValueFromPipelineByPropertyName,
+        HelpMessage='Name of file system folder to publish module to.'
+    )]
+    [string]$Path
+)
+
+
+Set-StrictMode -Version "Latest"
+
+
+# Run Tests
+Write-Verbose "Running Pester tests."
+[int]$failedTests = Invoke-Pester -Script "$PSScriptRoot\..\Tests\PowMark.Tests\" -EnableExit
+
+
+# Publish.
+if ($failedTests -eq 0) {
+
+    # Publish to file system.
+    if ($Path) {
+        try {
+
+            Write-Verbose "Publishing to: $Path."
+            Copy-Item -Path "$PSScriptRoot\..\Source\PowMark" -Destination $Path -Force -Recurse
+        }
+        catch {
+
+            throw "Cannot publish module to '$Path': $_."
+        }
+    }
+
+
+    # Publish to repo.
+    if ($Repository) {
+
+        try {
+
+            Write-Verbose "Publishing to: $Repository."
+            Publish-Module -Path "$PSScriptRoot\..\Source\PowMark" -Repository $Repository -Force
+        }
+        catch {
+
+            throw "Cannot publish module to '$Repository': $_"
+        }
+    }
+}
